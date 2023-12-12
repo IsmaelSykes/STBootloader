17fecca0ecdc67a0050a9d375e878ca4d0f66e20
IsmaelSykes <ismaelbarrera26@gmail.com>
2023-12-12 10:39:45 -0600

    Bootloader Timer16, return in wait function and no debug

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -311,13 +311,13 @@ void wait(void)
 	 {
 			for(int z = 0; z <5; z++)
 			{
-				printf("\033\143");
-				printf("\r waiting ");
+				//printf("\033\143");
+				//printf("\r waiting ");
 				for(int q = 0; q<=z; q++)
 				{
-					printf(".");
+					//printf(".");
 				}
-				printf("\r\n");
+				//printf("\r\n");
 				HAL_Delay(10);
 				 if(flag_break)
 				 {
@@ -347,7 +347,7 @@ uint32_t update_firmware (void)
 		 clear_loader_rom();
 		 //CMD 1
 		 HAL_UART_Transmit(&huart2,"FZ\n", (sizeof("FZ\n")-1),500);// begin
-		 printf("\r Send FZ ... \r\n");
+		 //printf("\r Send FZ ... \r\n");
 		 wait();
 		 if(flag_break)
 		 {
@@ -361,48 +361,48 @@ uint32_t update_firmware (void)
 		pages = (uint32_t)(FW_SIZE/512);
 		n_bytes = 512*pages;
 		offset = FW_SIZE - n_bytes;
-		printf(" \r fw_size: %lX \n",FW_SIZE);
-		printf(" \r crc_app: %lX \n",CRC_16);
-		printf(" \r pages: %ld \n",pages);
-		printf(" \r n_bytes: %ld \n",n_bytes);
-		printf(" \r offset: %ld \n",offset);
+		//printf(" \r fw_size: %lX \n",FW_SIZE);
+		//printf(" \r crc_app: %lX \n",CRC_16);
+		//printf(" \r pages: %ld \n",pages);
+		//printf(" \r n_bytes: %ld \n",n_bytes);
+		//printf(" \r offset: %ld \n",offset);
 
 		HAL_Delay(1000);
 
-		printf("Send OK................. \n\r");
+		//printf("Send OK................. \n\r");
 		HAL_UART_Transmit(&huart2,"OK\n", (sizeof("OK\n")-1),500);
 		memset(rx_buff,'\0',sizeof(rx_buff));
 		wait();
 		 dato_recivido = false;
 	     transmit_data = true;
 
-		 printf(" \r Starting loader.......... \r\n");
+		 //printf(" \r Starting loader.......... \r\n");
 		while( j<=pages-1)
 		{
 			memcpy(&index_page,&rx_buff[0],4);
 			memcpy(&crc_part,&rx_buff[4],4);
-			printf(" \r index: %ld \n",index_page);
-			printf(" \r crc_part: %lX \n",crc_part);
+			//printf(" \r index: %ld \n",index_page);
+			//printf(" \r crc_part: %lX \n",crc_part);
 			crc = CRC16_X25(&rx_buff[8], 512, 0);
-			printf(" \r crc computed: %X \r\n",crc);
+			//printf(" \r crc computed: %X \r\n",crc);
 
 			if((crc == crc_part) && (index_page < pages))
 			{
 			  a = write(&rx_buff[8],a);
 			  crc_rec = CRC16_X25(&rx_buff[8], 512, crc_rec);
-			  printf(" \r crc_rec: %lX \r\n",crc_rec);
+			  //printf(" \r crc_rec: %lX \r\n",crc_rec);
 
 			  memset(buffer,'\0',sizeof(buffer));
 			  memset(strnum,'\0',sizeof(strnum));
 			  snprintf(strnum,sizeof(strnum), "%ld",index_page);
 			  memcpy(&buffer[0], OK, sizeof(OK));
 			  memcpy(&buffer[sizeof(OK)], strnum,strlen(strnum));
-			  printf(" %s\n\r",buffer);
+			  //printf(" %s\n\r",buffer);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)buffer, strlen(buffer),500);
 			  wait();
 			  dato_recivido = false;
 			  j++;
-			  printf(" j: %d \n\r",j);
+			  //printf(" j: %d \n\r",j);
 			}
 			else
 			{
@@ -413,8 +413,8 @@ uint32_t update_firmware (void)
 			  snprintf(strnum,sizeof(strnum), "%d",j);
 			  memcpy(&buffer[0], ERR, sizeof(ERR));
 			  memcpy(&buffer[sizeof(ERR)], strnum,strlen(strnum));
-			  printf(" \r %s\r\n",buffer);
-			  printf(" j: %d \n\r",j);
+			  //printf(" \r %s\r\n",buffer);
+			  //printf(" j: %d \n\r",j);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)buffer, strlen(buffer),500);
 			}
 
@@ -423,14 +423,14 @@ uint32_t update_firmware (void)
 
 		if(offset!=0)
 		{
-			printf(" \r --------------Last Page ----------- \n");
+			//printf(" \r --------------Last Page ----------- \n");
 			memcpy(&index_page,&rx_buff[0],4);
 			memcpy(&crc_part,&rx_buff[4],4);
-			printf(" \r index: %lX \n",index_page);
-			printf(" \r crc_part: %lX \n",crc_part);
+			//printf(" \r index: %lX \n",index_page);
+			//printf(" \r crc_part: %lX \n",crc_part);
 			crc = CRC16_X25(&rx_buff[8], offset, 0);//offset
-			printf(" \r crc computed: %X \r\n",crc);
-			printf(" \r crc_rec: %lX \r\n",crc_rec);
+			//printf(" \r crc computed: %X \r\n",crc);
+			//printf(" \r crc_rec: %lX \r\n",crc_rec);
 
 
 			if(crc == crc_part)
@@ -440,18 +440,18 @@ uint32_t update_firmware (void)
 			  memset(buffer,'\0',sizeof(buffer));
 			  memset(strnum,'\0',sizeof(strnum));
 			  snprintf(strnum,sizeof(strnum), "%d",index_page);
-			  printf("strnum: %s, len: %d \n",strnum,strlen(strnum));
+			  //printf("strnum: %s, len: %d \n",strnum,strlen(strnum));
 			  memcpy(&buffer[0], OK, sizeof(OK));
 			  memcpy(&buffer[sizeof(OK)], strnum,strlen(strnum));
-			  printf(" \r buffer: %s\r\n",buffer);
+			  //printf(" \r buffer: %s\r\n",buffer);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)buffer, strlen(buffer),500);
 
 			  cmd7[0] = crc_rec;
 			  cmd7[1] = 0xFFFFFFFF;
-			  printf("\r ************************************* \r\n");
-			  printf(" \r crc rec: %lX \r\n",crc_rec);
-			  printf(" \r crc_app: %lX \n",CRC_16);
-			  printf("\r ************************************* \r\n");
+			  //printf("\r ************************************* \r\n");
+			  //printf(" \r crc rec: %lX \r\n",crc_rec);
+			  //printf(" \r crc_app: %lX \n",CRC_16);
+			  //printf("\r ************************************* \r\n");
 			  HAL_Delay(200);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)cmd7, sizeof(cmd7),500);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)cmd7, sizeof(cmd7),500);
@@ -466,7 +466,7 @@ uint32_t update_firmware (void)
 			  snprintf(strnum,sizeof(strnum), "%d",index_page);
 			  memcpy(&buffer[0], ERR, sizeof(ERR));
 			  memcpy(&buffer[sizeof(ERR)], strnum,strlen(strnum));
-			  printf(" \r buffer: %s\r\n",buffer);
+			  //printf(" \r buffer: %s\r\n",buffer);
 			  HAL_UART_Transmit(&huart2,(uint8_t*)buffer, strlen(buffer),500);
 				  //return -1;
 			}
@@ -475,10 +475,10 @@ uint32_t update_firmware (void)
 	//--------------------- Check Integration -----------------
 		if(CRC_16 == crc_rec)
 		{
-			printf("\r ************* CRC OK ****************** \r\n");
-			printf(" \r crc rec: %lX \r\n",crc_rec);
-			printf(" \r crc_app: %lX \n",CRC_16);
-			printf("\r ************************************* \r\n");
+			//printf("\r ************* CRC OK ****************** \r\n");
+			//printf(" \r crc rec: %lX \r\n",crc_rec);
+			//printf(" \r crc_app: %lX \n",CRC_16);
+			//printf("\r ************************************* \r\n");
 
 			HAL_Delay(2000);
 			cmd7[0] = crc_rec;
@@ -491,10 +491,10 @@ uint32_t update_firmware (void)
 		}
 		else
 		{
-			printf("\r ************* CRC FAIL ****************** \r\n");
-			printf(" \r crc rec: %lX \r\n",crc_rec);
-			printf(" \r crc_app: %lX \n",CRC_16);
-			printf("\r ************************************* \r\n");
+			//printf("\r ************* CRC FAIL ****************** \r\n");
+			//printf(" \r crc rec: %lX \r\n",crc_rec);
+			//printf(" \r crc_app: %lX \n",CRC_16);
+			//printf("\r ************************************* \r\n");
 
 			HAL_Delay(2000);
 			cmd7[0] = 0xFFFFFFFF;
@@ -511,30 +511,30 @@ uint32_t update_firmware (void)
 		hdr = image_get_header(IMAGE_SLOT_2);//magic
 		if (hdr == NULL)
 		{
-			printf("Magic incorrect \r\n");
+			//printf("Magic incorrect \r\n");
 			err =  -1;
 		}
 		if (image_validate(IMAGE_SLOT_2, hdr) != 0)//crc
 		{
-			printf("CRC incorrect \r\n");
+			//printf("CRC incorrect \r\n");
 			err = -1;
 		}//*/
 	// -------------------------- Clone ----------------------------
 
 		if((CRC_16 == crc_rec) && (err == 0))
 		{
-			printf("Ready to write to  Slot 1 \r\n");
+			//printf("Ready to write to  Slot 1 \r\n");
 			HAL_Delay(2000);
 			err = clone_rom(FW_SIZE);
 		}
 		if ( err == 0)
 		{
-			printf("UPDATE SUCCESSFULLY\r\n");
+			//printf("UPDATE SUCCESSFULLY\r\n");
 			return 0;
 		}
 		else
 		{
-			printf("UPDATE FAIL\r\n");
+			//printf("UPDATE FAIL\r\n");
 			return -1;
 		}//*/
 
@@ -658,24 +658,24 @@ int main(void)
 	{
 		HAL_Delay(5);
 		boot = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_14);// 1/0
-		printf("\r boot: %d \r\n",boot);
+		//printf("\r boot: %d \r\n",boot);
 		bootloader = timer_flag & boot;
 		if(bootloader)
 		{
-			printf(" App \n\r");
+			//printf(" App \n\r");
 			HAL_TIM_Base_Stop_IT(&htim1);
 			shared_mem_set_app_update_requested(false);
 		}
 		 if(bootloader == 0)
 		{
-			printf(" \r Bootloader \r\n");
+			//printf(" \r Bootloader \r\n");
 			HAL_TIM_Base_Stop_IT(&htim1);
 			shared_mem_set_app_update_requested(true);
 		}
 
 	    if((flag_break == 1) && (bootloader==0))
 		{
-			printf(" \r App Timer \r\n");
+			//printf(" \r App Timer \r\n");
 			shared_mem_set_app_update_requested(false);
 		}
 
@@ -685,7 +685,7 @@ int main(void)
 		// Load the updater (apparom)
 		if (hdr == NULL)
 		{
-			printf("No image found in slot 2\r\n");
+			//printf("No image found in slot 2\r\n");
 		}
 		else
 		{
@@ -696,9 +696,9 @@ int main(void)
 		//ioport_set_pin_level(RED_LED, false);
 		//ioport_set_pin_level(GREEN_LED, false);
 		//ioport_set_pin_level(BLUE_LED, false);
-		printf("Jumping to updater\r\n");
+		//printf("Jumping to updater\r\n");
 		shared_mem_increment_boot_counter();
-		printf("Boot count: %d \r\n",shared_mem_get_boot_counter());
+		//printf("Boot count: %d \r\n",shared_mem_get_boot_counter());
 		image_start(hdr);
 
 	}
@@ -709,7 +709,7 @@ int main(void)
 		if (shared_mem_get_boot_counter() >= max_boot_attemps)
 		{
 			shared_mem_clear_boot_counter();
-			printf("App unstable, entering to DFU mode\r\n");
+			//printf("App unstable, entering to DFU mode\r\n");
 			break;
 		}
 
@@ -718,7 +718,7 @@ int main(void)
 		// Load the app (apparom)
 		if (hdr == NULL)
 		{
-			printf("No image found in slot 1\r\n");
+			//printf("No image found in slot 1\r\n");
 			goto invalid;
 			break;
 		}
@@ -733,9 +733,9 @@ int main(void)
 		//ioport_set_pin_level(GREEN_LED, false);
 		//ioport_set_pin_level(BLUE_LED, false);
 
-		printf("Jumping to application\r\n\n");
+		//printf("Jumping to application\r\n\n");
 		shared_mem_increment_boot_counter();
-		printf("Boot count: %d \r\n",shared_mem_get_boot_counter());
+		//printf("Boot count: %d \r\n",shared_mem_get_boot_counter());
 		image_start(hdr);
 	}
 
@@ -745,21 +745,21 @@ int main(void)
 		if (update_firmware() == 0)
 		{
 			//Clean the variables
-			printf("Update completed, restarting\r\n");
+			//printf("Update completed, restarting\r\n");
 			shared_mem_set_update_completed();
 			shared_mem_clear_boot_counter();
 			shared_mem_clear_ota_info();
 			shared_mem_set_app_update_requested(false);
 			shared_mem_set_update();
-			printf("shared_mem_get_update: %d \n\r",shared_mem_get_update());
+			//printf("shared_mem_get_update: %d \n\r",shared_mem_get_update());
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, 0);//resetea el uC
 			HAL_Delay(2000);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, 1);
-			printf("Reset COMM \n\r");
+			//printf("Reset COMM \n\r");
 			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, 0);//resetea el uC
 			HAL_Delay(2000);
 			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, 1);
-			printf("Shutdown COMM �?� \n\r");
+			//printf("Shutdown COMM �?� \n\r");
 
 			HAL_NVIC_SystemReset();
 		}
@@ -1195,12 +1195,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
    if(htim->Instance == TIM15)
      {
-		printf("TIMER15\r\n\n");
-		printf("flag_timer: %d\r\n\n",flag_timer);
+		//printf("TIMER15\r\n\n");
+		//printf("flag_timer: %d\r\n\n",flag_timer);
 		if(flag_timer)
 		{
 			flag_break = 1;
-			printf("flag_break: %d\r\n\n",flag_break);
+			//printf("flag_break: %d\r\n\n",flag_break);
 		}
 		flag_timer = 1;
 
